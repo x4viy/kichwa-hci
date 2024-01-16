e07b80823d2dcff8aafb17c620aaad85d822f902
AnthonyR <nathael343@gmail.com>
2024-01-16 15:45:45 -0500

    ADD: Socket conection on clasificationGame

--- apps/clasificacion/consumers.py
+++ apps/clasificacion/consumers.py
@@ -22,18 +22,21 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         json_text = json.loads(text_data)
         message = json_text["message"]
+        motion = json_text["motion"]
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 "type": "chat_message",
-                "message": message
+                "message": message,
+                "motion": motion
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        motion = event['motion']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps({"message": message, "motion": motion}))
--- apps/clasificacion/views.py
+++ apps/clasificacion/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.db import connection
+from apps.gen.forms import *
 
 # Create your views here.
 # Create your views here.
 def index(request):
-    return render(request, 'Juegos-Multimedia/index.html')
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """ SELECT c.cart_descripcion AS DESCRIPCION, ma.muar_ruta AS RUTA, ma.muar_tipo AS TIPO
+                FROM gen.carta c 
+                INNER JOIN gen.carta_mult cm ON cm.camu_cart_id = c.cart_id
+                INNER JOIN gen.multimedia_archivos ma ON ma.muar_id = cm.camu_muar_id 
+                WHERE c.cart_estado = 1
+                """,
+        )
+
+        data = dictfetchall(cursor)
+
+    for i in data:
+        i['descripcion'] = i['descripcion'].replace(' ', '-')
+    data[0]["url"] = "url de carta"
+    context = {'segment': 'Juego de Memoria', 'datos': data }
+    return render(request, 'Juegos-Multimedia/clasification-game.html', context)
